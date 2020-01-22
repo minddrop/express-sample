23227783974c0ecfe34217082f057d95e60cf0d6
minddrop <ryooooooo1@gmail.com>
2020-01-23 07:15:20 +0900

    mock user page

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import express from 'express'
 import morgan from 'morgan'
 import path from 'path'
 import indexRouter from './routes/index'
+import usersRouter from './routes/users'
 
 const app = express()
 app.set('views', path.resolve('src', 'views'))
@@ -10,5 +11,6 @@ app.set('view engine', 'pug')
 app.use(morgan('tiny'))
 app.use(express.static(path.resolve('src', 'public')))
 app.use('/', indexRouter)
+app.use('/users', usersRouter)
 
 export default app
